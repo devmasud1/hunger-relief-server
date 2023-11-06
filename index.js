31f910c80f2938f996791488a31fcc9db30401d1
devmasud1 <devmahmud.masu1@gmail.com>
2023-11-06 23:49:22 +0600

    user added food api

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aunb3y8.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,21 +25,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const foodCollection = client.db('hungerRelief').collection('foods');
-    const foodRequestCollection = client.db('hungerRelief').collection('foodRequests');
+    const foodCollection = client.db("hungerRelief").collection("foods");
+    const foodRequestCollection = client
+      .db("hungerRelief")
+      .collection("foodRequests");
 
-    app.get('/api/v1/foods', async(req, res) => {
-        const result = await foodCollection.find().toArray();
-        res.send(result);
-    })
+    //foods api here
+    app.get("/api/v1/foods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/api/v1/food/:id', async(req, res) => {
+    app.get("/api/v1/food/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await foodCollection.findOne(query);
       res.send(result);
-    })
+    });
+    //foods api end
 
+    //add food by user api
+    app.post("/api/v1/food", async (req, res) => {
+      const addFood = req.body;
+      const result = await foodCollection.insertOne(addFood);
+      res.send(result);
+    });
+
+    //add food by user api end
 
     //Food Request api start here
     app.post("/api/v1/food-request", async (req, res) => {
@@ -49,28 +60,37 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/api/v1/food-request', async(req, res) => {
+    app.get("/api/v1/food-request", async (req, res) => {
       let query = {};
       if (req.query?.email) {
-        query = { user_email: req.query.email};
+        query = { user_email: req.query.email };
 
         const options = {
-          projection: { donar_name: 1, pickup_location: 1, expired_date: 1, request_date: 1, donation_money: 1, status: 1 },
+          projection: {
+            donar_name: 1,
+            pickup_location: 1,
+            expired_date: 1,
+            request_date: 1,
+            donation_money: 1,
+            status: 1,
+          },
         };
 
-        const result =await foodRequestCollection.find(query, options).toArray();
+        const result = await foodRequestCollection
+          .find(query, options)
+          .toArray();
         res.send(result);
       }
-    })
+    });
 
-    app.delete('/api/v1/food-request/:id', async (req, res) => {
+    app.delete("/api/v1/food-request/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await foodRequestCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.patch('/api/v1/food-request/:id', async (req, res) => {
+    app.patch("/api/v1/food-request/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedStatus = req.body;
@@ -95,11 +115,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hunger-Relief is available here')
-})
+app.get("/", (req, res) => {
+  res.send("Hunger-Relief is available here");
+});
 
 app.listen(port, () => {
-  console.log(`Hunger-Relief listening on port ${port}`)
-})
+  console.log(`Hunger-Relief listening on port ${port}`);
+});
